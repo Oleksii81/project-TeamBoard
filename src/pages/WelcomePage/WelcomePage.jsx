1e31fe6a12b63ba3ddd8d88b34230b57665adf37
Andy <andyfartushniy@gmail.com>
2024-02-02 19:35:23 +0200

    Created WelcomePage

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,16 +1,22 @@
-
-
-
-
-
-
-
-
-
+import image from '../../images/Image 1.png';
+import icon from '../../images/icon.svg';
+import { StyledWelcomePage, WelcomeButton } from './WelcomePageStyle';
 
 
 const WelcomePage = () => {
-
-}
+    return (
+      <StyledWelcomePage>
+        <img src={image} alt="Icon" />
+        <h1>Task Pro</h1>
+        <img src={icon} alt="Icon" />
+        <p>
+          Supercharge your productivity and take control of your tasks with Task
+          Pro - Don't wait, start achieving your goals now!
+        </p>
+        <WelcomeButton type="button">Registration</WelcomeButton>
+        <WelcomeButton type="button">Log In</WelcomeButton>
+      </StyledWelcomePage>
+    );
+};
 
 export default WelcomePage;
---
+++ src/pages/WelcomePage/WelcomePageStyle.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledWelcomePage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  width: 473px;
+  height: 438px;
+  margin-top: 166px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const WelcomeButton = styled.button`
+  border-radius: 8px;
+  width: 344px;
+  height: 49px;
+  background: #161616;
+`;
