7af68f504adb91236c9d447139b138af191c752f
Andy <andyfartushniy@gmail.com>
2024-02-02 22:38:33 +0200

    Updated welcomePage

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,22 +1,27 @@
-import image from '../../images/Image 1.png';
-import icon from '../../images/icon.svg';
-import { StyledWelcomePage, WelcomeButton } from './WelcomePageStyle';
-
+import sprite from '../../images/sprite.svg';
+import mainImage from '../../images/Image 1.png'
+import { StyledLogo, StyledWelcomePage, Link, Container } from './WelcomePageStyle';
 
 const WelcomePage = () => {
-    return (
+  return (
+    <Container>
       <StyledWelcomePage>
-        <img src={image} alt="Icon" />
-        <h1>Task Pro</h1>
-        <img src={icon} alt="Icon" />
+        <img src={mainImage} alt="Icon" />
+        <StyledLogo>
+          <svg width={48} height={48}>
+            <use href={`${sprite}#icon-logo`}></use>
+          </svg>
+          <h1>Task Pro</h1>
+        </StyledLogo>
         <p>
           Supercharge your productivity and take control of your tasks with Task
           Pro - Don't wait, start achieving your goals now!
         </p>
-        <WelcomeButton type="button">Registration</WelcomeButton>
-        <WelcomeButton type="button">Log In</WelcomeButton>
+        <Link href="/register">Registration</Link>
+        <Link href="/login">Log In</Link>
       </StyledWelcomePage>
-    );
+    </Container>
+  );
 };
 
 export default WelcomePage;
--- src/pages/WelcomePage/WelcomePageStyle.jsx
+++ src/pages/WelcomePage/WelcomePageStyle.jsx
@@ -1,4 +1,12 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 770px;
+  max-width: 1440px;
+  padding-top: 166px;
+  background-image: linear-gradient(to bottom, white, rgba(190, 219, 176, 1));
+`;
 
 export const StyledWelcomePage = styled.div`
   display: flex;
@@ -8,14 +16,34 @@ export const StyledWelcomePage = styled.div`
   text-align: center;
   width: 473px;
   height: 438px;
-  margin-top: 166px;
   margin-left: auto;
   margin-right: auto;
 `;
 
-export const WelcomeButton = styled.button`
+export const Link = styled(NavLink)`
   border-radius: 8px;
   width: 344px;
   height: 49px;
-  background: #161616;
+  color: #161616;
+  text-decoration: none;
+
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+  font-size: 14px;
+  letter-spacing: -0.02em;
+  text-align: center;
+
+  &.hover {
+    color: #fff;
+    background: #161616;
+  }
 `;
+
+export const StyledLogo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 24px 0 8px 0;
+  gap: 14px;
+  height: 60px;
+`
