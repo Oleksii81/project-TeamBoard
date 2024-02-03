9095f35754d7f50cdf9cb9b772acbf077d84947c
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2024-02-04 01:07:13 +0200

    brought back the routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,10 @@
-// import WelcomePage from 'pages/WelcomePage/WelcomePage';
-// import { Route, Routes } from 'react-router';
-import CreateBord from '../../src/components/Sidebar/CreateBord/CreateBord';
+import WelcomePage from 'pages/WelcomePage/WelcomePage';
+import { Route, Routes } from 'react-router';
 
 export const App = () => {
   return (
-    <CreateBord />
-
-    // <Routes>
-    //   <Route path="/" element={<WelcomePage />} />
-    // </Routes>
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+    </Routes>
   );
 };
