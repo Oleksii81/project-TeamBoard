217a0a5ce36c8e7104e3c2387e6a5432a92b393b
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-02-04 12:50:42 +0100

    add sitebar

--- src/components/Sidebar/BordList/BordList.jsx
+++ src/components/Sidebar/BordList/BordList.jsx
@@ -1,17 +1,16 @@
-
-
-
-
-
-
-
-
-
-
-
+import { LogoComponent, TaskPro } from './BordList.styled';
+import icons from '../../../images/sprite.svg';
 
 const BordList = () => {
+  return (
+    <LogoComponent>
+      <svg width="32" height="32">
+        <use href={`${icons}#icon-logo`}></use>
+      </svg>
 
-}
+      <TaskPro>Task Pro</TaskPro>
+    </LogoComponent>
+  );
+};
 
 export default BordList;
---
+++ src/components/Sidebar/Sidebar.styled.jsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+export const SideBarMenu = styled.div`
+  padding: 24px;
+  width: 260px;
+  height: 100vh;
+  background-color: rgba(18, 18, 18, 0.8);
+  /* background-color: teal; */
+  transition: transform 0.3s linear;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow: auto;
+
+  // Tablet
+  @media screen and (max-width: 767px) {
+    top: 0;
+    overflow: auto;
+    position: fixed;
+    left: -310px;
+    z-index: 100;
+    &.active {
+      transform: translateX(100%);
+      z-index: 10;
+    }
+  }
+  // Mobile
+  @media screen and (max-width: 375px) {
+    max-width: 225px;
+    width: 225px;
+    left: -275px;
+  }
+`;
+
+export const ButtonBurger = styled.button`
+  display: none;
+  @media screen and (max-width: 767px) {
+    width: 32px;
+    height: 32px;
+    background-color: blueviolet;
+    display: block;
+    position: fixed;
+    top: 18px;
+    left: 32px;
+    z-index: 0;
+    &.active {
+      display: none;
+    }
+  }
+`;
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5); // Затемнение заднего фона
+  display: none;
+
+  &.active {
+    display: block;
+    z-index: 9; // Помещаем поверх сайдбара
+  }
+`;
+
+export const SvgBurger = styled.svg`
+  position: fixed;
+  top: 18px;
+  left: 32px;
+`;
