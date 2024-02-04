d7c434cf8edae9e9a87921a8a342e95cce08e2cd
Andy <andyfartushniy@gmail.com>
2024-02-04 23:24:26 +0200

    Fixed welcomPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
+import AuthPage from "pages/AuthPage/AuthPage";
 import WelcomePage from "pages/WelcomePage/WelcomePage";
 import { Route, Routes } from "react-router";
 
 export const App = () => {
   return (
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+        <Route path="/auth/:id" element={<AuthPage />} />
+    </Routes>
   );
 };
