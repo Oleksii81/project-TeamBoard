3883f43ac36879fe7d35c12e79f1875b423c26b1
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2024-02-04 21:03:03 +0000

    header & themes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
-import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+
 
 import GlobalStyle from 'GlobalStyle';
 
