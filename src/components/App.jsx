6e6640054a744be0f66d69347e2030a3755ca543
Anton240896 <antonalipov791@gmail.com>
2024-02-05 12:11:30 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
+import AuthPage from 'pages/AuthPage/AuthPage';
+import WelcomePage from 'pages/WelcomePage/WelcomePage';
+import { Route, Routes } from 'react-router';
+
 export const App = () => {
-  return;
+  return (
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/auth/:id" element={<AuthPage />} />
+    </Routes>
+  );
 };
