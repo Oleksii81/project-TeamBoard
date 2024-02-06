fdaa695ce96c5c0443eb39dbf0ceb6587cdb0e0a
Kovalchuk63 <vov4uk4763@gmail.com>
2024-02-06 13:01:41 +0200

    add board

--- src/components/ScreensPage/Column/Column.jsx
+++ src/components/ScreensPage/Column/Column.jsx
@@ -1,16 +1,81 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import EllipsisText from 'react-ellipsis-text';
 
+import ModalColumn from 'components/Modals/ModalColumn/ModalColumn';
+import ModalConfirm from 'shared/components/modalConfirm/ModalConfirm';
 
+import { deleteColumn } from 'redux/task/taskOperations';
+import { getColumn } from 'redux/task/taskSelectors';
+import icons from '../../images/sprite.svg';
+import { BoardItem, IconEdit, IconEditCustom, ProgName } from './Сolumn.styled';
 
+const Column = ({ title, columnId }) => {
+  const dispatch = useDispatch();
 
+  const [showModal, setShowModal] = useState(false);
+  const [showConfirm, setShowConfirm] = useState(false);
 
+  const getColumns = useSelector(getColumn);
+  const tasksInColumn = getColumns.filter(el => el._id === columnId);
 
+  const handleOpen = () => {
+    setShowConfirm(true);
+  };
 
+  const handleClose = () => {
+    setShowConfirm(false);
+  };
 
+  const onOpen = () => {
+    setShowModal(true);
+  };
+  const onClose = () => {
+    setShowModal(false);
+  };
 
+  const handleDelete = () => {
+    dispatch(deleteColumn(columnId));
+    setShowConfirm(false);
+  };
 
-
-const Column = () => {
-
-}
+  return (
+    <BoardItem>
+      <ProgName>
+        <div>
+          <EllipsisText text={title} length={'30'} />
+        </div>
+      </ProgName>
+      <IconEditCustom>
+        <IconEdit onClick={onOpen}>
+          <use href={`${icons}#icon-pencil`}></use>
+        </IconEdit>
+        <IconEdit onClick={handleOpen}>
+          <use href={`${icons}#icon-trash`}></use>
+        </IconEdit>
+      </IconEditCustom>
+      {showModal && (
+        <ModalColumn
+          onClose={onClose}
+          title="Edit column"
+          btnName="Edit"
+          columnTitle={title}
+          column={columnId}
+        />
+      )}
+      {showConfirm && (
+        <ModalConfirm
+          onClose={handleClose}
+          onConfirm={handleDelete}
+          title={
+            tasksInColumn[0]?.tasks?.length
+              ? 'There are tasks inside the column. Are you sure to delete this column?'
+              : 'Are you sure?'
+          }
+        />
+      )}
+    </BoardItem>
+  );
+};
 
 export default Column;
