127423d26cb8674f78ffdf4a0b0279677b950d4e
Anton240896 <antonalipov791@gmail.com>
2024-02-06 11:40:17 +0200

    add new branch

--- src/components/Sidebar/NeedHelp/NeedHelp.jsx
+++ src/components/Sidebar/NeedHelp/NeedHelp.jsx
@@ -1,17 +1,49 @@
+import { useState } from 'react';
 
+import {
+  ContainerHelp,
+  ImgHelp,
+  TextHelp,
+  IconHelp,
+  QuestionHelp,
+  BtnNeedHelp,
+} from './NeedHelp.style.js';
 
-
-
-
-
-
-
-
-
-
+import ModalNeedHelp from '../../Modals/ModalNeedHelp/ModalNeedHelp.jsx';
+import image from 'images/imagesNeedHelp/2.png';
+import icons from '../../../images/sprite.svg';
 
 const NeedHelp = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-}
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <ContainerHelp>
+      <ImgHelp src={image} alt="Help" />
+
+      <TextHelp>
+        If you need help with
+        <span style={{ color: '#bedbb0' }}> TaskPro</span>, check out our
+        support resources or reach out to our customer support team.
+      </TextHelp>
+
+      <BtnNeedHelp>
+        <IconHelp>
+          <use href={`${icons}#icon-help`}></use>
+        </IconHelp>
+        <QuestionHelp onClick={openModal}> Need help? </QuestionHelp>
+      </BtnNeedHelp>
+
+      <ModalNeedHelp isOpen={isModalOpen} onClose={closeModal} />
+    </ContainerHelp>
+  );
+};
 
 export default NeedHelp;
