054dbaa1c5c361bc93aa28224ee968ff4e0416a1
Anton240896 <antonalipov791@gmail.com>
2024-02-06 16:45:28 +0200

    improve image

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,14 @@
-// import AuthPage from 'pages/AuthPage/AuthPage';
-// import WelcomePage from 'pages/WelcomePage/WelcomePage';
-// import { Route, Routes } from 'react-router';
+import AuthPage from 'pages/AuthPage/AuthPage';
+import WelcomePage from 'pages/WelcomePage/WelcomePage';
+import { Route, Routes } from 'react-router';
 // import SideBar from './Sidebar/Sidebar';
-
-import NeedHelp from "./Sidebar/NeedHelp/NeedHelp";
 
 export const App = () => {
   return (
-    // <Routes>
-    //   <Route path="/" element={<WelcomePage />} />
-    //   <Route path="/auth/:id" element={<AuthPage />} />
-    // </Routes>
-    < NeedHelp/>
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/auth/:id" element={<AuthPage />} />
+    </Routes>
     // <>
     //   <SideBar />
     // </>
