9d9e15c685dc0f18f9e0347342e4d577f173a037
Andy <andyfartushniy@gmail.com>
2024-02-07 20:13:04 +0200

    Changed validation

--- src/pages/AuthPage/LoginForm.jsx
+++ src/pages/AuthPage/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { Form, Formik } from 'formik';
-import { FormContainer, Input, StyledButton, StyledFilds, StyledLink, StyledLinks, StyledSvg } from './LoginFormStyled';
+import { EmailErrorMessage, FormContainer, Input, PassErrorMessage, StyledButton, StyledFilds, StyledLink, StyledLinks, StyledSvg } from './LoginFormStyled';
 import sprite from '../../images/sprite.svg';
 import { useState } from 'react';
 import { login } from '../../redux/auth/authOperations';
@@ -7,10 +7,27 @@ import * as yup from 'yup';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { setToken } from '../../redux/auth/authSelectors';
+import { toast } from 'react-toastify';
 
 const schema = yup.object().shape({
-  email: yup.string().required(),
-  password: yup.string().required(),
+  email: yup
+    .string()
+    .email('Invalid email')
+    .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+      message: 'Email error',
+      excludeEmptyString: true,
+    })
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .max(64, 'Password must be no more than 64 characters')
+    .matches(/^[a-zA-Z0-9]+$/, {
+      message:
+        'Password must contain Latin letters and numbers, without spaces in different case, from 8 to 64 characters',
+      excludeEmptyString: true,
+    })
+    .required('Password is required'),
 });
 
 const LoginForm = () => {
@@ -28,7 +45,7 @@ const LoginForm = () => {
     const response = await dispatch(login(currentUser, setToken))
     
     if (response.error) {
-      alert(response.payload);
+      toast.error(response.payload);
     } else {
       navigate('/home');
     }
@@ -50,30 +67,41 @@ const LoginForm = () => {
         onSubmit={handleSubmit}
         validationSchema={schema}
       >
-        <Form>
-          <StyledFilds>
-            <Input type="email" name="email" placeholder="Enter your email" required />
-            <Input
-              type={showPassword ? 'text' : 'password'}
-              name="password"
-              placeholder="Confirm a password"
-              required
-            />
+        {({ handleChange, values }) => (
+          <Form>
+            <StyledFilds>
+              <Input
+                type="email"
+                name="email"
+                placeholder="Enter your email"
+                value={values.email || ''}
+                onChange={handleChange('email')}
+              />
+              <EmailErrorMessage name="email" component="div" />
+              <Input
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+                placeholder="Confirm a password"
+                value={values.password || ''}
+                onChange={handleChange('password')}
+              />
+              <PassErrorMessage name="password" component="div" />
 
-            <StyledSvg onClick={onPassVisible}>
-              {showPassword ? (
-                <svg width={18} height={18}>
-                  <use stroke="gray" href={`${sprite}#icon-eye`} />
-                </svg>
-              ) : (
-                <svg width={18} height={18}>
-                  <use href={`${sprite}#icon-eye-hiden`} />
-                </svg>
-              )}
-            </StyledSvg>
-          </StyledFilds>
-          <StyledButton type="submit">Log In Now</StyledButton>
-        </Form>
+              <StyledSvg onClick={onPassVisible}>
+                {showPassword ? (
+                  <svg width={18} height={18}>
+                    <use stroke="gray" href={`${sprite}#icon-eye`} />
+                  </svg>
+                ) : (
+                  <svg width={18} height={18}>
+                    <use href={`${sprite}#icon-eye-hiden`} />
+                  </svg>
+                )}
+              </StyledSvg>
+            </StyledFilds>
+            <StyledButton type="submit">Log In Now</StyledButton>
+          </Form>
+        )}
       </Formik>
     </FormContainer>
   );
--- src/pages/AuthPage/Registration.jsx
+++ src/pages/AuthPage/Registration.jsx
@@ -1,16 +1,37 @@
 import { Form, Formik } from "formik";
-import { FormContainer, Input, StyledButton, StyledFilds, StyledLink, StyledLinks, StyledSvg } from "./RegisterFormStyled";
+import { EmailErrorMessage, FormContainer, Input, NameErrorMessage, PassErrorMessage, StyledButton, StyledFilds, StyledLink, StyledLinks, StyledSvg } from "./RegisterFormStyled";
 import sprite from "../../images/sprite.svg";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { register } from "../../redux/auth/authOperations";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const schema = yup.object().shape({
-  userName: yup.string().required('Name is required'),
-  email: yup.string().required(),
-  password: yup.string().required(),
+  userName: yup
+    .string()
+    .min(2, 'Name is too short!')
+    .max(32, 'Name is too long!')
+    .required('Name is req!'),
+  email: yup
+    .string()
+    .email('Invalid email')
+    .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+      message: 'Email error',
+      excludeEmptyString: true,
+    })
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .max(64, 'Password must be no more than 64 characters')
+    .matches(/^[a-zA-Z0-9]+$/, {
+      message:
+        'Password must contain Latin letters and numbers, without spaces in different case, from 8 to 64 characters',
+      excludeEmptyString: true,
+    })
+    .required('Password is required'),
 });
 
 const Registration = () => {
@@ -28,29 +49,30 @@ const Registration = () => {
     const response = await dispatch(register(newUser))
 
     if (response.error) {
-      alert(response.payload)
+      toast.error(response.payload)
     } else {
       navigate('/home');
     }
     resetForm();
    };
   
-    return (
-      <FormContainer>
-        <StyledLinks>
-          <StyledLink to="/auth/register">Registration</StyledLink>
-          <StyledLink to="/auth/login">Log In</StyledLink>
-        </StyledLinks>
+  return (
+    <FormContainer>
+      <StyledLinks>
+        <StyledLink to="/auth/register">Registration</StyledLink>
+        <StyledLink to="/auth/login">Log In</StyledLink>
+      </StyledLinks>
 
-        <Formik
-          initialValues={{
-            userName: '',
-            email: '',
-            password: '',
-          }}
-          onSubmit={handleSubmit}
-          validationSchema={schema}
-        >
+      <Formik
+        initialValues={{
+          userName: '',
+          email: '',
+          password: '',
+        }}
+        onSubmit={handleSubmit}
+        validationSchema={schema}
+      >
+        {({ handleChange, values }) => (
           <Form>
             <StyledFilds>
               <Input
@@ -58,21 +80,26 @@ const Registration = () => {
                 name="userName"
                 placeholder="Enter your name"
                 pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
+                value={values.userName || ''}
+                onChange={handleChange('userName')}
               />
+              <NameErrorMessage name="userName" component="div" />
               <Input
                 type="email"
                 name="email"
                 placeholder="Enter your email"
-                required
+                value={values.email || ''}
+                onChange={handleChange('email')}
               />
+              <EmailErrorMessage name="email" component="div" />
               <Input
                 type={showPassword ? 'text' : 'password'}
                 name="password"
                 placeholder="Create your password"
-                required
+                value={values.password || ''}
+                onChange={handleChange('password')}
               />
+              <PassErrorMessage name="password" component="div" />
 
               <StyledSvg onClick={onPassVisible}>
                 {showPassword ? (
@@ -88,9 +115,10 @@ const Registration = () => {
             </StyledFilds>
             <StyledButton type="submit">Register Now</StyledButton>
           </Form>
-        </Formik>
-      </FormContainer>
-    );
+        )}
+      </Formik>
+    </FormContainer>
+  );
 };
 
 export default Registration;
