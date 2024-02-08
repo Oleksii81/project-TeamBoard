5160b167cc017fa30476a8126a84767fe0d92c98
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2024-02-08 07:45:55 +0000

    burger

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,28 @@
-
-
-
-
-
-
-
+import Header from 'components/Header/Header';
+import ScreensPage from 'components/ScreensPage/ScreensPage';
+import Sidebar from 'components/Sidebar/Sidebar';
+import { useState } from 'react';
 
 
 
 const HomePage = () => {
+     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+
+    return (
+        <>
+        <Sidebar isSidebarOpen={isSidebarOpen} onCloseSideBar={closeSidebar}/>
+        <Header  toggleSidebar={toggleSidebar}/>
+        <ScreensPage />
+          
+        </>
+      );
 }
 
 export default HomePage;
