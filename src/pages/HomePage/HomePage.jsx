356d549f050c013166976e42f9eab953540572c2
AntoninaYatsenko <dp130688jaj@gmail.com>
2024-02-08 17:55:31 +0100

    error

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,24 @@
-
-import SideBar from '../../components/Sidebar/Sidebar';
-import Header from '../../components/Header/Header';
-import ScreensPage from '../../components/ScreensPage/ScreensPage';
+import Header from 'components/Header/Header';
+import ScreensPage from 'components/ScreensPage/ScreensPage';
+import Sidebar from 'components/Sidebar/Sidebar';
+import { useState } from 'react';
 
 const HomePage = () => {
-   return (
-     <>
-       <SideBar />
-       <ScreensPage />
-       <Header />
-     </>
-   ); 
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  return (
+    <>
+      <Sidebar isSidebarOpen={isSidebarOpen} onCloseSideBar={closeSidebar} />
+      <Header toggleSidebar={toggleSidebar} />
+      <ScreensPage />
+    </>
+  );
 };
 
 export default HomePage;
