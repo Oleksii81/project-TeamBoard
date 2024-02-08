a8e79075d9bd2e0961c567c184badd9052358d9a
AntoninaYatsenko <dp130688jaj@gmail.com>
2024-02-08 17:46:30 +0100

    error

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,16 @@
 
-
-
-
-
-
-
-
-
+import SideBar from '../../components/Sidebar/Sidebar';
+import Header from '../../components/Header/Header';
+import ScreensPage from '../../components/ScreensPage/ScreensPage';
 
 const HomePage = () => {
-
-}
+   return (
+     <>
+       <SideBar />
+       <ScreensPage />
+       <Header />
+     </>
+   ); 
+};
 
 export default HomePage;
