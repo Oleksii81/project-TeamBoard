d597bd04597e547375bb86cf6421599f092a9f21
AntoninaYatsenko <dp130688jaj@gmail.com>
2024-02-08 18:31:50 +0100

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,17 @@ import WelcomePage from 'pages/WelcomePage/WelcomePage';
 import { Route, Routes } from 'react-router';
 import HomePage from 'pages/HomePage/HomePage';
 
+// import SideBar from '../../src/components/Sidebar/Sidebar';
+// import Header from '../../src/components/Header/Header';
+// import ScreensPage from '../../src/components/ScreensPage/ScreensPage';
 
 export const App = () => {
   return (
-   
+    // <>
+    //   <SideBar />
+    //   <ScreensPage />
+    //   <Header />
+    // </>
     <Routes>
       <Route path="/" element={<WelcomePage />} />
       <Route path="/auth/:id" element={<AuthPage />} />
