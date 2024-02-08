abf6438ccfd4d129223780358ce53892a1f2fc63
Anton240896 <antonalipov791@gmail.com>
2024-02-08 14:25:31 +0200

    Update ModalNeedHelp.jsx

--- src/components/Modals/ModalNeedHelp/ModalNeedHelp.jsx
+++ src/components/Modals/ModalNeedHelp/ModalNeedHelp.jsx
@@ -2,8 +2,6 @@ import FormNeedHelp from '../../Sidebar/NeedHelp/FormNeedHelp';
 import { OverlayModal } from './ModalNeedHelp.styled';
 import ReactModal from 'react-modal';
 
-
-
 ReactModal.setAppElement('#root');
 
 const ModalNeedHelp = ({ isOpen, onClose }) => {
