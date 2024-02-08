b1c2bd45af3b80bc59c569be041c6a22f87001df
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2024-02-08 09:21:44 +0000

    finished sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,28 +1,12 @@
 import {
   SideBarMenu,
-  // ButtonBurger,
   Overlay,
-  /*   SvgBurger,
-  BurgerPath, */
 } from './Sidebar.styled';
-//import LogoComponent from './LogoComponent/LogoComponent';
-//import CreateBord from '../../../src/components/Sidebar/CreateBord/CreateBord';
-//import BoadrsList from './BoardsList/BoardsList';
 import NeedHelp from './NeedHelp/NeedHelp';
 import LogOut from './LogOut/LogOut';
 import React from 'react';
 
 const SideBar = ({ isSidebarOpen, onCloseSideBar }) => {
-  /*  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-*/
-  /*  const closeSidebar = () => {
-    isSidebarOpen = false;
-  } */
-
   return (
     <>
       <Overlay
@@ -30,18 +14,6 @@ const SideBar = ({ isSidebarOpen, onCloseSideBar }) => {
         onClick={onCloseSideBar}
       />
       <SideBarMenu className={` ${isSidebarOpen ? 'active' : ''}`}>
-        {/* <ButtonBurger
-          className={isSidebarOpen ? 'active' : ''}
-          onClick={toggleSidebar}
-        >
-          <BurgerPath d="M4 16h24" />
-          <BurgerPath d="M4 8h24" />
-          <BurgerPath d="M4 24h24" />
-        </ButtonBurger>
-
-        <LogoComponent />
-        <CreateBord />
-        {/* <BoadrsList /> */}
         <NeedHelp />
         <LogOut />
       </SideBarMenu>
