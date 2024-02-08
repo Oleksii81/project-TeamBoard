d8388dcda5b654212b1232041aff8c811c1680d9
Anton240896 <antonalipov791@gmail.com>
2024-02-08 15:20:14 +0200

    Update ModalNeedHelp.jsx

--- src/components/Modals/ModalNeedHelp/ModalNeedHelp.jsx
+++ src/components/Modals/ModalNeedHelp/ModalNeedHelp.jsx
@@ -1,6 +1,20 @@
-import FormNeedHelp from '../../Sidebar/NeedHelp/FormNeedHelp';
-import { OverlayModal } from './ModalNeedHelp.styled';
 import ReactModal from 'react-modal';
+import FormNeedHelp from '../../Sidebar/NeedHelp/FormNeedHelp';
+
+const stylesModal = {
+  content: {
+    position: 'fixed',
+    inset: '0',
+    top: '0',
+    width: '100%',
+    height: '100%,',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'var(--helpBgColor)',
+    zIndex: 120,
+  },
+};
 
 ReactModal.setAppElement('#root');
 
@@ -8,14 +22,17 @@ const ModalNeedHelp = ({ isOpen, onClose }) => {
  
   return (
      
-    <OverlayModal
+    <ReactModal
       isOpen={isOpen}
       onRequestClose={onClose}
+      style={stylesModal}
+      contentLabel="Image Modal"
     >
       <FormNeedHelp closeModal={onClose}/>
-      </OverlayModal>
+      </ReactModal>
   );
 };
 
 
 export default ModalNeedHelp;
+
