68ee0b85e0be362d227373f4e63bda9c32113fd2
Oleksii81 <tourus81@gmail.com>
2024-02-10 13:05:01 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { PrivateRoute, RestrictedRoute } from './AuthRoutes';
 import { refreshUser } from '../redux/auth/authOperations';
 import { isLogin, isRefreshing } from '../redux/auth/authSelectors';
 import { isLoading } from '../redux/task/taskSelectors';
-import Loader from './Loader/Loader';
+import Loader from '../components/Loader/Loader';
 
 const WelcomePage = lazy(() => import('pages/WelcomePage/WelcomePage'));
 const AuthPage = lazy(() => import('pages/AuthPage/AuthPage'));
