a69e600419ffa3275a25379fa3fef91cd64b039f
Kateryna-gr <kategrytsyk@gmail.com>
2024-02-12 01:03:05 +0200

    back-screens-page

--- src/components/ScreensPage/ScreensPage.jsx
+++ src/components/ScreensPage/ScreensPage.jsx
@@ -1,45 +1,42 @@
-// import { Container } from './ScreensPage.styled';
-// import { useState } from 'react';
-// import { useSelector } from 'react-redux';
-// import Text from '../../../src/components/ScreensPage/Text/Text';
-// import Filter from '../../../src/components/ScreensPage/Filter/Filter';
-// import Bord from '../../../src/components/ScreensPage/Bord/Bord';
-// import { getBoard } from '../../../src/redux/auth/authSelectors';
-import CadrsList from './CardList/CardsList';
+import { Container } from './ScreensPage.styled';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import Text from '../../../src/components/ScreensPage/Text/Text';
+import Filter from '../../../src/components/ScreensPage/Filter/Filter';
+import Bord from '../../../src/components/ScreensPage/Bord/Bord';
+import { getBoard } from '../../../src/redux/auth/authSelectors';
 
 const ScreensPage = () => {
-  // const boards = useSelector(getBoard);
+  const boards = useSelector(getBoard);
 
-  // const [filterByPriority, setFilterByPriority] = useState('');
-  // // const [cards, setCards] = useState([]);
-  // const onFilterChange = newFilter => {
-  //   console.log(newFilter);
-  //   setFilterByPriority(newFilter);
-  //   console.log(filterByPriority);
-  // };
+  const [filterByPriority, setFilterByPriority] = useState('');
+  // const [cards, setCards] = useState([]);
+  const onFilterChange = newFilter => {
+    console.log(newFilter);
+    setFilterByPriority(newFilter);
+    console.log(filterByPriority);
+  };
 
   // const visibleCards = cards.filter(card => card.priority === filterByPriority);
 
-  // return boards && boards.length ? (
-  //   <section>
-  //     <Bord
-  //       setFilterByPriority={setFilterByPriority}
-  //       onChangeFilter={onFilterChange}
-  //     />
-  //   </section>
-  // ) : (
-  //   <section>
-  //     <Container>
-  //       <Filter
-  //         setFilterByPriority={setFilterByPriority}
-  //         onChangeFilter={onFilterChange}
-  //       />
-  //       <Text />
-  //     </Container>
-  //   </section>
-  // );
-
-  return <CadrsList />;
+  return boards && boards.length ? (
+    <section>
+      <Bord
+        setFilterByPriority={setFilterByPriority}
+        onChangeFilter={onFilterChange}
+      />
+    </section>
+  ) : (
+    <section>
+      <Container>
+        <Filter
+          setFilterByPriority={setFilterByPriority}
+          onChangeFilter={onFilterChange}
+        />
+        <Text />
+      </Container>
+    </section>
+  );
 };
 
 export default ScreensPage;
