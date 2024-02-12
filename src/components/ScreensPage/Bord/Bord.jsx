2aa18d3bfe99f46d1c49d9fdeacc28c9aa96fa4f
Andy <andyfartushniy@gmail.com>
2024-02-12 19:51:01 +0200

    Added cardlist demo

--- src/components/ScreensPage/Bord/Bord.jsx
+++ src/components/ScreensPage/Bord/Bord.jsx
@@ -1,11 +1,13 @@
 import { AddBoardBtn } from '../../../../src/components/ScreensPage/AddBoardBtn/AddBoardBtn';
 import Filter from '../../../../src/components/ScreensPage/Filter/Filter';
+import CardList from '../Card/CardList';
 import { ContainerMain } from './Bord.styled';
 
 const Bord = ({ onChangeFilter, setFilterByPriority }) => {
   return (
     <>
       <ContainerMain>
+        <CardList filterByPriority={null}/>
         <AddBoardBtn />
         <Filter
           onChangeFilter={onChangeFilter}
